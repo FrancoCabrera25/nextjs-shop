92060818f7b45527341ebdc9613d7ed54ea3efe1
Franco Cabrera <64378629+FrancoCabrera25@users.noreply.github.com>
2022-04-17 17:36:02 -0300

    principal layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
+import { Typography } from '@mui/material'
 import type { NextPage } from 'next'
+import { ShopLayout } from '../components/layouts'
 
 const Home: NextPage = () => {
   return (
-    <h1>SHOP</h1>
+   <ShopLayout title='SHOP' pageDescription='compra los mejores productos'>
+      <Typography variant='h1' component='h1'>Home Tienda </Typography>
+      <Typography variant='h2' sx={{ mb: 1 }}>Todos los productos </Typography>
+   </ShopLayout>
   )
 }
 
