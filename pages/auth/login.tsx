ca5ac5e42c6422b0f32f28943463e008e38d1867
Franco Cabrera <franco.cabrera@naranja.com>
2022-04-26 15:50:47 -0300

    ajustes en login

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -9,23 +9,23 @@ const LoginPage: NextPage = () => {
     <AuthLayout title="Login">
       <Box sx={{ width: 350, padding: "10px 20px" }}>
         <Grid container spacing={2}>
-          <Grid item xs={12}>
+          <Grid item xs={12} display= 'flex' justifyContent='center' sx= {{ my: 2 }}>
             <Typography variant="h1" component="h1">
               Iniciar Sesión
             </Typography>
           </Grid>
           <Grid item xs={12}>
-            <TextField label="Email" variant="filled" fullWidth />
+            <TextField label="Email" variant="standard" fullWidth />
           </Grid>
           <Grid item xs={12}>
             <TextField
               label="Password"
               type="password"
-              variant="filled"
+              variant="standard"
               fullWidth
             />
           </Grid>
-          <Grid item xs={12}>
+          <Grid item xs={12} sx={{ mt: 2 }}>
             <Button color="secondary" className="circular-btn" size="large" fullWidth>
               Ingresar
             </Button>
