914c6ed5183759d2a8e9c6f7c87ee0f609fc615d
Franco Cabrera <franco.cabrera@naranja.com>
2022-04-28 22:04:28 -0300

    get product by slug

---
+++ pages/api/products/[slug].ts
@@ -0,0 +1,37 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { db, SHOP_CONSTANTS } from "../../../database";
+import { IProduct } from "../../../interface";
+import { Product } from "../../../models";
+
+type Data = { message: string } | IProduct;
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  switch (req.method) {
+    case "GET":
+      return getProductBySlug(req, res);
+
+    default:
+      return res.status(400).json({ message: "Bad request" });
+  }
+}
+
+const getProductBySlug = async (
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) => {
+  const { slug } = req.query;
+
+  await db.connect();
+
+  const product = await Product.findOne({ slug }).lean();
+  await db.disconnect();
+
+  if (product) {
+    return res.status(200).json(product);
+  }
+
+  return res.status(400).json({ message: "Producto no encontrado" });
+};
