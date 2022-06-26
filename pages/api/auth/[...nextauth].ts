235d83c58aadb9348e0567422fecff210ef60da1
Franco Cabrera <franco.cabrera@naranja.com>
2022-06-26 19:47:42 -0300

    config secret

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -21,6 +21,7 @@ export default NextAuth({
     })
     // ...add more providers here
   ],
+  secret: process.env.NEXTAUTH_SECRET,
   pages: {
     signIn: '/auth/login',
     newUser: '/auth/register'
